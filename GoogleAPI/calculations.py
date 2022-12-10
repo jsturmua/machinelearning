cb98fad26e3cdb96a4e89f7a8ef14e6ec98c0d9e
Johnny <sturmjoni@googlemail.com>
2022-12-10 23:22:30 +0100

    added calculations

--- GoogleAPI/calculations.py
+++ GoogleAPI/calculations.py
@@ -1,9 +1,16 @@
+import sys
+sys.path.append("/usr/local/lib/python3.8/dist-packages")
+import googlemaps
+
 def calculate_routes(startlocation, stations):
+    gmaps = googlemaps.Client(key='AIzaSyCj8S_a8wIqEiv0gx8XfVtdSJqDXvJb3jo')
     distances = []
-    origins = ["Perth, Australia",]
+    origins = [startlocation,]
     destinations = []
     i = 0
     tempstations = []
     for station in stations:
         i = i+1
         tempstations.append(station)
+        temp_destination_string = station.name + "[" + station.location.y + ", " + station.location.x + "]"
+        destinations.append(temp_destination_string)
